d5876c13dacdfeadd4e66b9047dfb42c18507564
Satoshi Terajima <sleepy.st818@gmail.com>
2014-05-31 13:30:02 +0900

    ToDoを完了できる。

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
+)
+
+const (
+	Doing    = iota
+	Complete = iota
 )
 
 type ToDo struct {
 	Registerd time.Time // 登録日時
 	Content   string    // ToDoの内容
+	Status    int       // 状態（実行中, 完了）
 }
 
 func newToDo(t time.Time, content string) *ToDo {
-	return &ToDo{t, content}
+	return &ToDo{t, content, Doing}
 }
 
 type ToDoList struct {
@@ -23,10 +30,6 @@ func NewToDoList() *ToDoList {
 	return &ToDoList{&items}
 }
 
-func (t *ToDoList) Count() int {
-	return len(*t.items)
-}
-
 func (t *ToDoList) Add(regTime time.Time, content string) {
 	todo := newToDo(regTime, content)
 	temp := append(*t.items, *todo)
@@ -34,11 +37,40 @@ func (t *ToDoList) Add(regTime time.Time, content string) {
 }
 
 func (t *ToDoList) Get(no int) (bool, *ToDo) {
-	if no <= t.Count() {
-		return true, &(*t.items)[no]
+	doings := t.GetByStatus(Doing)
+
+	if no <= len(doings) {
+		return true, &(doings[no])
 	}
 
 	return false, nil
+}
+
+func (t *ToDoList) GetByStatus(status int) []ToDo {
+	items := make([]ToDo, 0)
+	for _, item := range *t.items {
+		if item.Status == status {
+			items = append(items, item)
+		}
+	}
+	return items
+}
+
+func (t *ToDoList) Complete(no int) error {
+	i := 0
+	for j, todo := range *t.items {
+		if todo.Status != Doing {
+			continue
+		}
+		if no != i {
+			continue
+		}
+
+		(*t.items)[j] = ToDo{todo.Registerd, todo.Content, Complete}
+		return nil
+	}
+
+	return errors.New("noが範囲外")
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -10,11 +10,15 @@ import (
 func TestTodoListCanAddTodo(t *testing.T) {
 	lst := main.NewToDoList()
 
-	assert.Equal(t, lst.Count(), 0, "ToDoリスト生成時は0件")
+	doingCount := func() int {
+		return len(lst.GetByStatus(main.Doing))
+	}
+
+	assert.Equal(t, doingCount(), 0, "ToDoリスト生成時は0件")
 
 	lst.Add(time.Now(), "test")
 
-	assert.Equal(t, lst.Count(), 1, "ToDoを追加したので1件")
+	assert.Equal(t, doingCount(), 1, "ToDoを追加したので1件")
 
 	exists, todo := lst.Get(0)
 	if !exists {
@@ -23,4 +27,14 @@ func TestTodoListCanAddTodo(t *testing.T) {
 	}
 
 	assert.Equal(t, todo.Content, "test", "追加したToDo内容を参照できる")
+}
+
+func TestTodoCanComplete(t *testing.T) {
+	lst := main.NewToDoList()
+	lst.Add(time.Now(), "test")
+
+	err := lst.Complete(0)
+	assert.Equal(t, err, nil, "ERROR")
+	assert.Equal(t, len(lst.GetByStatus(main.Doing)), 0, "ToDo完了時はステータスはDoingではない")
+	assert.Equal(t, len(lst.GetByStatus(main.Complete)), 1, "ToDo完了時はステータスはComplete")
 }
