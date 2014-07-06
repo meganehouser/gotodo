3a0ffae1f6bf18e950a5bc600e8b18f6e1a31b00
Satoshi Terajima <sleepy.st818@gmail.com>
2014-07-06 23:47:49 +0900

    useing json for persisting tasks

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os/user"
+	"path"
+	"strconv"
 	"time"
 )
 
@@ -11,12 +18,23 @@ const (
 	Complete = iota
 )
 
+var StoreFile string = path.Join(getUserHome(), "tasks.json")
+
+func getUserHome() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return usr.HomeDir
+}
+
 type ToDo struct {
 	Registerd time.Time // 登録日時
 	Content   string    // ToDoの内容
 	Status    int       // 状態（実行中, 完了）
 }
 
+// ユーザーディレクトリのパス取得
 func newToDo(t time.Time, content string) *ToDo {
 	return &ToDo{t, content, Doing}
 }
@@ -28,6 +46,37 @@ type ToDoList struct {
 func NewToDoList() *ToDoList {
 	items := make([]ToDo, 0)
 	return &ToDoList{items}
+}
+
+func LoadToDoList(path string) *ToDoList {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return NewToDoList()
+	}
+
+	todoitems := make([]ToDo, 0)
+	err = json.Unmarshal(data, &todoitems)
+	if err != nil {
+		panic(err)
+	}
+
+	tdlist := NewToDoList()
+	tdlist.items = todoitems
+	return tdlist
+}
+
+func SaveToDoList(data *ToDoList, path string) {
+	jsn, err := json.Marshal((*data).items)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = ioutil.WriteFile(path, jsn, 0600)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }
 
 func (t *ToDoList) Add(regTime time.Time, content string) {
@@ -49,9 +98,11 @@ func (t *ToDoList) Complete(no int) error {
 	i := 0
 	for j, todo := range t.items {
 		if todo.Status != Doing {
+			i = i + 1
 			continue
 		}
 		if no != i {
+			i = i + 1
 			continue
 		}
 
@@ -67,5 +118,32 @@ func (t *ToDoList) Clean() {
 }
 
 func main() {
-	fmt.Println("Hello world")
+	ls := flag.Bool("ls", false, "list up tasks")
+	clean := flag.Bool("clean", false, "clean up completed tasks")
+	completeNo := flag.String("finish", "", "complete the task")
+	add := flag.String("add", "", "add task")
+
+	flag.Parse()
+
+	data := *LoadToDoList(StoreFile)
+	defer SaveToDoList(&data, StoreFile)
+
+	if *ls {
+		items := data.GetByStatus(Doing) 
+		for i, item := range items {
+			fmt.Printf("[%d] [%s] %s\r\n", i, item.Registerd.Format("2006/01/02"), item.Content)
+		}
+	} else if *clean {
+		data.Clean()
+	} else if *completeNo != "" {
+		no, err := strconv.Atoi(*completeNo)
+		if err != nil {
+			panic(err)
+		}
+
+		data.Complete(no)
+	} else if *add != "" {
+		data.Add(time.Now(), *add)
+	}
+
 }
